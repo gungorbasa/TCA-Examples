73a18498c0573e17a66bd7136a976c0e60a8866a
Gungor Basa <gungorbasa@gmail.com>
2023-04-02 18:41:50 +0300

    Initialize TabBar example

---
+++ TabBar/TabBar/TabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarApp.swift
+//  TabBar
+//
+//  Created by Gungor Basa on 4/2/23.
+//
+
+import SwiftUI
+
+@main
+struct TabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
